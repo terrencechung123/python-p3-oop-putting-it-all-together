30eb7a8f497d795a41a546b730db5b9c24acc506
Terrence Chung <terrencechung35@gmail.com>
2023-02-23 20:01:08 -0800

    lab

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
-
 class Book:
-    pass
+    def __init__(self, title, author='', page_count=0, genre=''):
+        self.title = title
+        self.author = author
+        self.page_count = page_count
+        self.genre = genre
+
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_page_count(self, value):
+        if type(value) == int:
+            self._page_count = value
+
+        print('page_count must be an integer')
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+    
+    page_count = property(get_page_count, set_page_count)
+
+
+
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size=0):
+        self.brand = brand
+        self.size = size
+
+    def get_shoe_size(self):
+        return self._size
+    
+    def set_shoe_size(self, value):
+        if type(value) == int:
+            self._size = value
+
+        print("size must be an integer")
+
+    def cobble(self):
+        self.condition = "New"
+        print('Your shoe is as good as new!')
+        
+
+    size = property(get_shoe_size, set_shoe_size)
